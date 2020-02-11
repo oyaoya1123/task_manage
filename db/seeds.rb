064c0189d0367e3b0467b6c22bcea36501490f60
oyaoya1123 <oyaror@yahoo.co.jp>
2020-02-11 11:17:10 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "test_user",
+  email: "test1@test.co.jo",
+  password:              "A-123456",
+  password_confirmation: "A-123456")
